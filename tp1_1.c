b69e00457acb9401972d75d71415d3506d06c5c1
GonzaJM <gonzalojamardotuc@gmail.com>
2025-04-01 22:54:56 -0300

    hago uso de punteros

--- tp1_1.c
+++ tp1_1.c
@@ -2,6 +2,14 @@
 
 int main(){
     printf("hola mundo");
+    int variable = 23;
+    int *punt = &variable;
+
+    printf("\n%d ", *punt);
+    printf("\n%p ", &punt);
+    printf("\n%p ", &variable);
+    printf("\n%zu ", sizeof(variable));
+
 
     return 0;
 }

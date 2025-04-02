fe65ee9ea4af41be2b78bb705bdf9c3811e2b129
GonzaJM <gonzalojamardotuc@gmail.com>
2025-04-01 23:51:47 -0300

    ejercicio 2.4 completado

--- tp1_2.c
+++ tp1_2.c
@@ -2,16 +2,25 @@
 
 int cuadrado(int x);
 void cuadradoVacio(int x);
+void usoVariable(int x);
+void invertir(int a, int b);
+void orden(int a, int b);
 
 int main(){
 
-    int numero = 3, numero_cuad;
+    int numero = 3, numero_cuad, nro1 = 2, nro2 = 4;
 
     numero_cuad = cuadrado(numero);
 
     printf("\n%d ", numero_cuad);
 
     cuadradoVacio(numero);
+
+    usoVariable(numero);
+
+    invertir(nro1, nro2);
+
+    orden(nro1, nro2);
 
     return 0;
 }
@@ -30,4 +39,28 @@ void cuadradoVacio(int x){
     int i, cuad = 0;
 
     printf("\n%d ", x*x);
+}
+
+void usoVariable(int x){
+    printf("\n%p ", &x);
+    printf("\n%d", x);
+}
+
+void invertir(int a, int b){
+    int burbuja;
+    burbuja = a;
+    a = b;
+    b = burbuja;
+    printf("\n%d ", a);
+    printf("\n%d ", b);
+}
+
+void orden(int a, int b){
+    if(a < b){
+        printf("\n valor mas chico: %d", a);
+        printf("\n valor mas grande: %d", b);
+    } else{
+        printf("\n valor mas chico: %d", b);
+        printf("\n valor mas grande: %d", a);
+    }
 }
